622f49823e296f42cfb77f2a42ede6b26ddf0d0b
ch5225 <chaohua5225>
2022-03-06 15:29:15 +0800

    add splitDataset.py and train.py

--- utils/splitDataset.py
+++ utils/splitDataset.py
@@ -7,31 +7,35 @@ import random
 #
 #   当前该库将测试集当作验证集使用，不单独划分测试集
 # ----------------------------------------------------------------------#
-trainval_percent = 0.5
+trainval_percent = 0.6
 train_percent = 0.9
 # -------------------------------------------------------#
 #   指向数据集所在的文件夹
 #   默认指向根目录下的数据集
 # -------------------------------------------------------#
-dataset_path = ''
+dataset_path = '../dataset/oaisys_data'
 
 if __name__ == "__main__":
     random.seed(0)
     print("Generate txt in ImageSets.")
     segfilepath = os.path.join(dataset_path, 'rgb')
-    saveBasePath = os.path.join(dataset_path, 'VOC2007/ImageSets/Segmentation')
+    saveBasePath = os.path.join(dataset_path, 'ImageSets')
+    if not os.path.exists(saveBasePath):
+        os.mkdir(saveBasePath)
 
     temp_seg = os.listdir(segfilepath)
     total_seg = []
     for seg in temp_seg:
-        if seg.endswith(".png"):
+        if 'Left' in seg:
+            if random.randint(0, 1):
+                seg = seg.replace('Left', 'Right')
             total_seg.append(seg)
 
     num = len(total_seg)
-    list = range(num)
+    data_list = range(num)
     tv = int(num * trainval_percent)
     tr = int(tv * train_percent)
-    trainval = random.sample(list, tv)
+    trainval = random.sample(data_list, tv)
     train = random.sample(trainval, tr)
 
     print("train and val size", tv)
@@ -41,7 +45,7 @@ if __name__ == "__main__":
     ftrain = open(os.path.join(saveBasePath, 'train.txt'), 'w')
     fval = open(os.path.join(saveBasePath, 'val.txt'), 'w')
 
-    for i in list:
+    for i in data_list:
         name = total_seg[i][:-4] + '\n'
         if i in trainval:
             ftrainval.write(name)
